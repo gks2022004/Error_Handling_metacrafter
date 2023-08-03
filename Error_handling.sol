0ddb3807294174897323d6e47b375e68804f192d
Gaurav Kumar Singh <94637265+gks2022004@users.noreply.github.com>
2023-08-03 18:19:59 +0530

    Update Error_handling.sol

--- Error_handling.sol
+++ Error_handling.sol
@@ -1,28 +1,29 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ErrorHandlingContract {
-    function requireExample(uint256 x, uint256 y) public pure returns (uint256) {
-        // require statement
-        require(y != 0, "Division by zero is not allowed");
-        uint256 result = x / y;
-        return result;
-    }
-    
-    function assertExample(uint256 x, uint256 y) public pure returns (uint256) {
-        // assert statement
-        assert(y != 0);
-        uint256 result = x / y;
-        return result;
-    }
-    
-    function revertExample(uint256 x, uint256 y) public pure returns (uint256) {
-        // revert statement
-        if (y == 0) {
-            revert("Division by zero is not allowed");
-        }
-        uint256 result = x / y;
-        return result;
-    }
-}
+contract ErrorHandling {
 
+  mapping(address => uint) public balances;
+
+  function deposit(uint _amount) public {
+    balances[msg.sender] += _amount;
+  }
+
+  function withdrawRequire(uint _amount) public {
+    require(balances[msg.sender] >= _amount, "Insufficient balance");
+    balances[msg.sender] -= _amount;
+  }
+
+  function withdrawAssert(uint _amount) public {
+    assert(balances[msg.sender] >= _amount);
+    balances[msg.sender] -= _amount;
+  }
+
+  function withdrawRevert(uint _amount) public {
+    if(balances[msg.sender] < _amount) {
+      revert("Insufficient balance");
+    }
+    balances[msg.sender] -= _amount;
+  }
+
+}
